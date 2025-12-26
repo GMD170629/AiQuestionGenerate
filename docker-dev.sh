587702ef32e0e62fbb491b6448830eb8f370655a
六面体 <gamersguyu@gmail.com>
2025-12-26 17:58:33 +0800

    修改题目生成的模式

--- docker-dev.sh
+++ docker-dev.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # 开发环境启动脚本
+# 使用 profiles 方式启动开发环境服务，如果没有指定 profile 则默认启动开发环境
 
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml "$@"
+if [ -z "$COMPOSE_PROFILES" ]; then
+  docker-compose --profile dev "$@"
+else
+  docker-compose "$@"
+fi
 
--- docker-prod.sh
+++ docker-prod.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # 生产环境启动脚本
+# 使用 profiles 方式启动生产环境服务
 
-docker-compose -f docker-compose.yml -f docker-compose.prod.yml "$@"
+docker-compose --profile prod "$@"
 
